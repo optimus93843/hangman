9f0698111f072cbcbe0b24bca0a824e7b38a8176
Hassan <714303@pdsb.net>
2022-12-26 00:06:54 +0000

    Creating the structure of the game

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,30 @@
-print("Hello Hanger!!!")
+# import libraries 
+
+import random
+
+
+
+
+
+
+# Functions
+
+# This function will choose a word from our data base
+def get_word():
+
+# This function will play the game based on the word that has been selected
+def game(word):
+
+
+# This function will display the hanging man 
+  def display_hangman(tries):
+  
+
+
+
+# Main code
+
+while input("Do you want to play again? (Yes/No)").upper() == "YES":
+  #Play the game
+  print("playing the game")
+print("Good bye! Game is finished!!!")
